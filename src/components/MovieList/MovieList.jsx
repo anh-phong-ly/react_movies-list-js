13621fb3dcdacef31807d03028e31960e356775f
PhongLy <ly.phong165@gmail.com>
2025-01-29 14:40:09 +0700

    resolve

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,12 @@
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({movies}) => 
+(
+    <div className="movies">
+        {movies.map(movie => (
+            <MovieCar movie={movie} key={movie.imdbId}/>
+        ))}
+    </div>
+)
+ 
+
